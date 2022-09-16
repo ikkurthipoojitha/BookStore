1d3337197a8f75b58cee22c241f70d93fc2958b4
ikkurthipoojitha <ipoojitha1997@gmail.com>
2022-09-16 19:33:14 +0530

    updated js

--- src/App.js
+++ src/App.js
@@ -10,21 +10,34 @@ import BookDetails from './components/bookdetails/bookdetails';
 import MyCart from './components/cart/cart';
 import CartOrder from './components/cartorder/cartorder';
 import OrderDetails from './components/orderdetails/orderdetails';
+import Counter from './components/cart/counter';
+import RouterOne from './components/router/router';
+import Shopping from './components/shopping/shopping';
+
 function App() {
   return (
     <div className="App">
-      {/* < Login /> */}
+       {/* < Login />  */}
       {/* < Signup /> */}
       {/* < Lander /> */}
       {/* < Header /> */}
       {/* < Book /> */}
-      {/* < Dashboard />  */}
-       < MyCart />
+       {/* < Dashboard />   */}
+       {/* < MyCart />  */}
+       {/* < Counter/> */}
       {/* <BookDetails /> */}
       {/* < CartOrder /> */}
       {/* < OrderDetails/> */}
+       <RouterOne /> 
+       {/* < Shopping/> */}
     </div>
   );
 }
 
 export default App;
+
+
+
+
+// "email": "gorenityanand96@gmail.com",
+// "password": "Nitya@123",
