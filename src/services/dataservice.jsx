a97360b9b08e47fd7f3fae487ce31a9b35cc7da0
ikkurthipoojitha <ipoojitha1997@gmail.com>
2022-09-16 19:31:42 +0530

    updated data service

--- src/services/dataservice.jsx
+++ src/services/dataservice.jsx
@@ -1,9 +1,59 @@
 import axios from "axios";
 
 const headerConfig = {
-    headers : {Authorization:localStorage.getItem('accessToken')}
+    headers : {'x-access-token':localStorage.getItem('token')}
 }
 export const getBookList = () =>{
     let response = axios.get("https://bookstore.incubation.bridgelabz.com/bookstore_user/get/book",headerConfig)
+    return response
+}
+
+export const addWishList = (id) =>{
+    let response = axios.post(`https://bookstore.incubation.bridgelabz.com/bookstore_user/add_wish_list/${id}`, 
+    id,
+    headerConfig)
+    return response
+}
+export const getWishList = () =>{
+    let response = axios.get(`https://bookstore.incubation.bridgelabz.com/bookstore_user/get_wishlist_items`, 
+    headerConfig)
+    return response
+}
+export const addToCart = (id) =>{
+    let response = axios.post(`https://bookstore.incubation.bridgelabz.com/bookstore_user/add_cart_item/${id.product_id}`, 
+    id,
+    headerConfig)
+    return response
+}
+export const cartItemQuantity = (id,quantity) =>{
+    let response = axios.put(`https://bookstore.incubation.bridgelabz.com/bookstore_user/cart_item_quantity/${id}`, 
+ quantity,
+    headerConfig)
+    return response
+}
+
+export const getCartItem = () =>{
+    let response = axios.get("https://bookstore.incubation.bridgelabz.com/bookstore_user/get_cart_items",
+
+    headerConfig)
+    return response
+}
+
+export const deleteCartItem = (ob) =>{
+    let response = 
+    axios.delete(`https://bookstore.incubation.bridgelabz.com/bookstore_user/remove_cart_item/${ob.cartItem_id}`,
+    headerConfig)
+    return response
+}
+
+export const editUser = (userObj) =>{
+    let response = axios.put(`https://bookstore.incubation.bridgelabz.com/bookstore_user/edit_user`,
+    userObj,
+    headerConfig)
+    return response
+}
+export const addOrder = () =>{
+    let response = axios.post(`https://bookstore.incubation.bridgelabz.com/bookstore_user/add/order`,
+    headerConfig)
     return response
 }
