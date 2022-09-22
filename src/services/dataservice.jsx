396d9f8ffb44f5559583791451a3db9ed0dbc37d
ikkurthipoojitha <ipoojitha1997@gmail.com>
2022-09-22 14:03:10 +0530

    updated data service

--- src/services/dataservice.jsx
+++ src/services/dataservice.jsx
@@ -4,19 +4,28 @@ const headerConfig = {
     headers : {'x-access-token':localStorage.getItem('token')}
 }
 export const getBookList = () =>{
-    let response = axios.get("https://bookstore.incubation.bridgelabz.com/bookstore_user/get/book",headerConfig)
+    let response = 
+    axios.get("https://bookstore.incubation.bridgelabz.com/bookstore_user/get/book",headerConfig)
     return response
 }
 
 export const addWishList = (id) =>{
-    let response = axios.post(`https://bookstore.incubation.bridgelabz.com/bookstore_user/add_wish_list/${id}`, 
+    let response = 
+    axios.post(`https://bookstore.incubation.bridgelabz.com/bookstore_user/add_wish_list/${id.product_id}`, 
     id,
     headerConfig)
     return response
 }
 export const getWishList = () =>{
-    let response = axios.get(`https://bookstore.incubation.bridgelabz.com/bookstore_user/get_wishlist_items`, 
+    let response = 
+    axios.get(`https://bookstore.incubation.bridgelabz.com/bookstore_user/get_wishlist_items`, 
     headerConfig)
+    return response
+}
+export const deleteWishList = (obje) =>{
+    let response = 
+    axios.delete(`https://bookstore.incubation.bridgelabz.com/bookstore_user/remove_wishlist_item/${obje.cartItem_id}`, 
+     headerConfig)
     return response
 }
 export const addToCart = (id) =>{
@@ -26,7 +35,8 @@ export const addToCart = (id) =>{
     return response
 }
 export const cartItemQuantity = (id,quantity) =>{
-    let response = axios.put(`https://bookstore.incubation.bridgelabz.com/bookstore_user/cart_item_quantity/${id}`, 
+    let response = 
+    axios.put(`https://bookstore.incubation.bridgelabz.com/bookstore_user/cart_item_quantity/${id}`, 
  quantity,
     headerConfig)
     return response
@@ -52,8 +62,8 @@ export const editUser = (userObj) =>{
     headerConfig)
     return response
 }
-export const addOrder = () =>{
+export const addOrder = (inputobj) =>{
     let response = axios.post(`https://bookstore.incubation.bridgelabz.com/bookstore_user/add/order`,
-    headerConfig)
+    inputobj,headerConfig)
     return response
 }
